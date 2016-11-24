47e14c3ab9743b2c939812741742aef290251553
Jay Dunkelberger <ldunkelberger@pivotal.io>
2016-11-24 13:26:02 -0800

    Add basic mechanism for acquiring and watching a lease

--- handlers/acquire_lease.go
+++ handlers/acquire_lease.go
@@ -2,15 +2,35 @@ package handlers
 
 import (
 	"encoding/json"
+	"net"
 	"net/http"
+	"time"
 
+	"github.com/coreos/flannel/pkg/ip"
 	"github.com/coreos/flannel/subnet"
 )
 
 type AcquireLeaseHandler struct{}
 
 func (h *AcquireLeaseHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	writeResponse(w, subnet.Lease{})
+	attrs := subnet.LeaseAttrs{}
+	err := json.NewDecoder(r.Body).Decode(&attrs)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	_, net, err := net.ParseCIDR("10.255.99.0/24")
+	if err != nil {
+		panic(err)
+	}
+
+	lease := subnet.Lease{
+		Subnet:     ip.FromIPNet(net),
+		Attrs:      attrs,
+		Expiration: time.Now().Add(24 * time.Hour),
+	}
+	writeResponse(w, lease)
 	return
 }
 
--- handlers/watch_leases.go
+++ handlers/watch_leases.go
@@ -9,6 +9,11 @@ import (
 type WatchLeasesHandler struct{}
 
 func (h *WatchLeasesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	writeResponse(w, subnet.LeaseWatchResult{})
+	// always write the current state to the lease watch result snapshot
+	lwResult := subnet.LeaseWatchResult{
+		Snapshot: []subnet.Lease{},
+		Cursor:   "1",
+	}
+	writeResponse(w, lwResult)
 	return
 }
